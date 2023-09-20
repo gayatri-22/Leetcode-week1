237fcdf5889e501e377868d010e220e0c01a0ebf
Gayatri <63839136+gayatri-22@users.noreply.github.com>
2023-09-20 08:12:57 +0530

    Time: 5 ms (60.91%), Space: 43.6 MB (58.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
+        /*int[] ans = new int[2];
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
@@ -9,6 +9,24 @@ class Solution {
             }
             }
         }
-        return ans;
+        return ans;*/
+
+        int[] ans = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int index=0;
+        for(int num:nums){
+            map.put(num,index);
+            index++;
+        }
+        
+         for(int i=0;i<nums.length;i++){
+            int c= target-nums[i];
+            if(map.containsKey(c) && map.get(c)!=i){
+              ans[0] = i;
+              ans[1] = map.get(c);  
+            }
+            //map.put(a,i);
+        }
+            return ans;
     }
 }
